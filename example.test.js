fb744145ab760a54d4ee8fe832d816680a38fa69
Ranjan1501 <ranjankumar1757@gmail.com>
2022-03-17 10:16:26 +0530

    added unit test

---
+++ example.test.js
@@ -0,0 +1,29 @@
+const assert = require("assert");
+describe("Water Management Test", () => {
+  it("should return 2", () => {
+    assert.equal(
+      {
+        ALLOT_WATER: 2(3, ":", 7),
+        ADD_GUESTS: 2,
+        ADD_GUESTS: 3,
+        BILL,
+      },
+      2400,
+      " ",
+      5215
+    );
+  });
+  it("should return 9", () => {
+    assert.equal(
+      {
+        ALLOT_WATER: 3(2, ":", 1),
+        ADD_GUESTS: 4,
+        ADD_GUESTS: 1,
+        BILL,
+      },
+      3000,
+      " ",
+      5750
+    );
+  });
+});
